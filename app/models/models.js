7a7b8f653e30278020db7d423d6c000908c3a129
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-09-28 18:16:46 -0500

    added order and get results functionality

--- app/models/models.js
+++ app/models/models.js
@@ -13,10 +13,30 @@ $(window).load(function() {
 		console.log(likeList);
 	});
 
+	// Like a restaurant
 	$('ul.restaurants').delegate("li div.restaurant-card", "click", function() {
 		var rName = $(this).children('p:first-child').html();
 		likeList[rName] += 1;
 		console.log(likeList[rName]);
+	});
+
+	// Get results
+	$('button.submit').click(function() {
+		var tuples = [];
+		for (var key in likeList) tuples.push([key, likeList[key]]);
+
+		tuples.sort(function(a, b) {
+		    a = a[1];
+		    b = b[1];
+		    return a > b ? -1 : (a < b ? 1 : 0);
+		});
+
+		for (var i = 0; i < tuples.length; i++) {
+		    var key = tuples[i][0];
+		    var value = tuples[i][1];
+
+		    console.log(key + ': '+ value);
+		}
 	});
 
 });
